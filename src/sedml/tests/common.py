415d1b8944b5c8e0f0ebf3ad801f47ca6b7da9f8
Mauro Silberberg <maurosilber@gmail.com>
2023-09-27 10:40:16 +0100

    Remove deprecated attributes from comparison
    Deprecated attributes are not recreated after the roundtrip.

--- src/sedml/tests/common.py
+++ src/sedml/tests/common.py
@@ -3,6 +3,10 @@ from ..xml import Element, is_comment
 
 float_bool_str = try_types(float, bool_parser, str)
 
+deprecated: dict[str, set[str]] = {
+    "{http://sed-ml.org/sed-ml/level1/version4}curve": {"logX", "logY"},
+}
+
 
 def compare_xml(x: Element, y: Element) -> bool:
     """Compare two XML trees."""
@@ -10,9 +14,16 @@ def compare_xml(x: Element, y: Element) -> bool:
     x_keys = set(xi for xi in x.attrib.keys())
     y_keys = set(yi for yi in y.attrib.keys())
     diff: set[str] = set.symmetric_difference(x_keys, y_keys)
+    # Deprecated elements are ignored during parsing
+    # and not recreated after the round trip.
+    # We remove those keys.
+    deprecated_keys = deprecated.get(x.tag, set())
+    diff = diff.difference(deprecated_keys)
     assert len(diff) == 0, diff
 
     for k, xv in x.attrib.items():
+        if k in deprecated_keys:
+            continue
         yv = y.attrib[k]
         assert float_bool_str(xv) == float_bool_str(yv), (k, xv, yv)
 
