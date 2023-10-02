7af5b96f9a1e8eb92e98c15bc6beaa97a81a9008
Mauro Silberberg <maurosilber@gmail.com>
2023-10-02 10:42:11 +0100

    Rename main functions

--- src/sedml/__init__.py
+++ src/sedml/__init__.py
@@ -1,18 +1,26 @@
-from pathlib import Path
-from typing import Callable
+from __future__ import annotations
+
+from os import PathLike
 
 from . import l1v1, l1v2, l1v3, l1v4, xml
 
-readers: dict[tuple[str, str], Callable] = {
-    ("1", "1"): l1v1.SEDML.from_xml_tree,
-    ("1", "2"): l1v2.SEDML.from_xml_tree,
-    ("1", "3"): l1v3.SEDML.from_xml_tree,
-    ("1", "4"): l1v4.SEDML.from_xml_tree,
+SEDML = l1v1.SEDML | l1v2.SEDML | l1v3.SEDML | l1v4.SEDML
+
+readers: dict[tuple[str, str], type[SEDML]] = {
+    ("1", "1"): l1v1.SEDML,
+    ("1", "2"): l1v2.SEDML,
+    ("1", "3"): l1v3.SEDML,
+    ("1", "4"): l1v4.SEDML,
 }
 
 
-def read(file: Path):
-    xml_tree = xml.fromstring(file.read_bytes())
+def load(file: PathLike):
+    with open(file, "rb") as f:
+        return loads(f.read())
+
+
+def loads(text: str | bytes):
+    xml_tree = xml.fromstring(text)
     level = xml_tree.attrib["level"]
     version = xml_tree.attrib["version"]
     try:
@@ -22,4 +30,36 @@ def read(file: Path):
             f"reader for SED-ML level {level} version {version} not implemented"
         )
     else:
-        return reader(xml_tree)
+        return reader.from_xml_tree(xml_tree)
+
+
+def dump(
+    sedml: SEDML,
+    file: PathLike,
+    *,
+    encoding="UTF-8",
+    xml_declaration: bool = True,
+    pretty_print: bool = True,
+) -> None:
+    bytes = dumps(
+        sedml,
+        encoding=encoding,
+        xml_declaration=xml_declaration,
+        pretty_print=pretty_print,
+    )
+    with open(file, "wb") as f:
+        f.write(bytes)
+
+
+def dumps(
+    sedml: SEDML,
+    *,
+    encoding="UTF-8",
+    xml_declaration: bool = True,
+    pretty_print: bool = True,
+) -> bytes:
+    return sedml.to_xml(
+        encoding=encoding,
+        xml_declaration=xml_declaration,
+        pretty_print=pretty_print,
+    )
